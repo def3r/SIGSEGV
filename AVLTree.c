73967f3e380e67578d914e8fefcb47b2a78b4910
horrifyingHorse <excuseplanation@gmail.com>
2024-09-01 12:00:27 +0530

    tedious and extremely inefficient implementation

--- AVLTree.c
+++ AVLTree.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct node {
-  struct node* left;
+  struct node *left;
   int n;
-  struct node* right;
+  struct node *right;
 };
 
-int postorderBalanceTraverse(struct node*);
+struct node *top = NULL;
+struct node *mid = NULL;
+struct node *btm = NULL; // bottom imbalanced node
+int score = 0;
 
-struct node* initTree () {
-  struct node* rootNode = (struct node*)malloc(sizeof(struct node));
+int postorderBalanceTraverse(struct node *);
+
+struct node *initTree() {
+  struct node *rootNode = (struct node *)malloc(sizeof(struct node));
 
   return rootNode;
 }
 
-struct node* createNewNode(int n) {
-  struct node* newNode = (struct node*)malloc(sizeof(struct node));
+struct node *createNewNode(int n) {
+  struct node *newNode = (struct node *)malloc(sizeof(struct node));
   newNode->n = n;
   newNode->left = NULL;
   newNode->right = NULL;
@@ -25,26 +29,12 @@ struct node* createNewNode(int n) {
   return newNode;
 }
 
-struct node* insertNode(struct node* rootNode, int n) {
-  if (rootNode == NULL) {
-    return createNewNode(n); 
-  }
-  
-  if (n < rootNode->n) {
-    rootNode->left = insertNode(rootNode->left, n);
-  } else {
-    rootNode->right = insertNode(rootNode->right, n);
-  }
-
-//  postorderBalanceTraverse(rootNode);
-//  printf("\n");
-  return rootNode;
-}
-
-int height(struct node* root) {
+int height(struct node *root) {
   int leftHeight = 0;
   int rightHeight = 0;
-  if (root == NULL) { return 1; }
+  if (root == NULL) {
+    return 1;
+  }
 
   leftHeight = 1 + height(root->left);
   rightHeight = 1 + height(root->right);
@@ -52,11 +42,13 @@ int height(struct node* root) {
   return leftHeight > rightHeight ? leftHeight : rightHeight;
 }
 
-int postorderBalanceTraverse(struct node* tree) {
+int postorderBalanceTraverse(struct node *tree) {
   int leftHeight = 0;
   int rightHeight = 0;
 
-  if (tree == NULL) { return 0; }
+  if (tree == NULL) {
+    return 0;
+  }
 
   leftHeight = 1 + postorderBalanceTraverse(tree->left);
   rightHeight = 1 + postorderBalanceTraverse(tree->right);
@@ -67,29 +59,143 @@ int postorderBalanceTraverse(struct node* tree) {
   return leftHeight > rightHeight ? leftHeight : rightHeight;
 }
 
-void inorderTraverse(struct node* tree) {
-  if (tree == NULL) { return; }
+struct node *insertNode(struct node **rootNode, int n) {
+  top = NULL;
+  mid = NULL;
+  btm = NULL;
 
-  inorderTraverse(tree->left);
+  if (*rootNode == NULL) {
+    top = createNewNode(n);
+    return top;
+  }
+
+  if (n < (*rootNode)->n) {
+    (*rootNode)->left = insertNode(&((*rootNode)->left), n);
+  } else {
+    (*rootNode)->right = insertNode(&((*rootNode)->right), n);
+  }
+
+  btm = mid;
+  mid = top;
+  top = *rootNode;
+
+  score = height(top->left) - height(top->right);
+  //  printf("val %d: %d| ", top->n, score);
+  //  printf("\n");
+  if (abs(score) > 1) {
+    if (score < -1) {
+      if (mid->left == btm) {
+        // RL rotation
+        top->right = btm->left;
+        mid->left = btm->right;
+        btm->left = top;
+        btm->right = mid;
+
+        *rootNode = btm;
+        top = btm;
+      } else {
+        // RR rotation
+        top->right = mid->left;
+        mid->left = top;
+
+        *rootNode = mid;
+        top = mid;
+        mid = btm;
+      }
+    } else if (score > 1) {
+      if (mid->right == btm) {
+        // LR rotation
+        top->left = btm->right;
+        mid->right = btm->left;
+        btm->left = mid;
+        btm->right = top;
+
+        *rootNode = btm;
+        top = btm;
+      } else {
+        // LL rotation
+        top->left = mid->right;
+        mid->right = top;
+
+        *rootNode = mid;
+        top = mid;
+        mid = btm;
+      }
+    }
+  }
+
+  //  if (mid) {
+  //    printf("val %d: %d| ", mid->n, height(mid->left) - height(mid->right));
+  //  }
+  //  if (btm) {
+  //    printf("val %d: %d| ", btm->n, height(btm->left) - height(btm->right));
+  //  }
+
+  // postorderBalanceTraverse(rootNode);
+
+  return *rootNode;
+}
+
+void inorderTraverse(struct node *tree) {
+  if (tree == NULL) {
+    return;
+  }
+
   printf("%d ", tree->n);
+  inorderTraverse(tree->left);
   inorderTraverse(tree->right);
 
   return;
 }
 
 int main() {
-  struct node* avlTree = NULL;
-  avlTree = insertNode(avlTree, 3);
-  insertNode(avlTree, 5);
-  insertNode(avlTree, 1);
-  insertNode(avlTree, 4);
-  insertNode(avlTree, 2);
-  insertNode(avlTree, 7);
-  insertNode(avlTree, 9);
-  insertNode(avlTree, 10);
+  struct node *avlTree = NULL;
 
-  inorderTraverse(avlTree); 
-  printf("\n");
+  // Final Test:
+  avlTree = insertNode(&avlTree, 21);
+  insertNode(&avlTree, 26);
+  insertNode(&avlTree, 30);
+  insertNode(&avlTree, 9);
+  insertNode(&avlTree, 4);
+  insertNode(&avlTree, 14);
+  insertNode(&avlTree, 28);
+  insertNode(&avlTree, 18);
+  insertNode(&avlTree, 15);
+  insertNode(&avlTree, 10);
+  insertNode(&avlTree, 2);
+  insertNode(&avlTree, 3);
+  insertNode(&avlTree, 7);
+
+  // LL
+  //  avlTree = insertNode(&avlTree, 3);
+  //  insertNode(&avlTree, 2);
+  //  insertNode(&avlTree, 1);
+
+  // RR
+  //  avlTree = insertNode(&avlTree, 5);
+  //  insertNode(&avlTree, 2);
+  //  insertNode(&avlTree, 9);
+  //  insertNode(&avlTree, 3);
+  //  insertNode(&avlTree, 6);
+  //  insertNode(&avlTree, 11);
+  //  insertNode(&avlTree, 10);
+  //  insertNode(&avlTree, 4);
+  //  insertNode(&avlTree, 12);
+  //  insertNode(&avlTree, 13);
+
+  //  insertNode(avlTree, 5);
+  //  insertNode(avlTree, 5);
+  //  insertNode(avlTree, 1);
+  //  insertNode(avlTree, 4);
+  //  insertNode(avlTree, 2);
+  //  insertNode(avlTree, 7);
+  //  insertNode(avlTree, 9);
+  //  insertNode(avlTree, 10);
+
+  inorderTraverse(avlTree);
+  printf("\n\n");
+  //  postorderBalanceTraverse(avlTree);
+  //  printf("\n");
 
   return 0;
 }
