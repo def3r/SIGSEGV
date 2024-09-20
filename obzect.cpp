e7f2101035fc577dc0872071c96a715a30ac1aeb
horrifyingHorse <excuseplanation@gmail.com>
2024-09-20 19:16:47 +0530

    finally an application of inheritance

--- obzect.cpp
+++ obzect.cpp
@@ -1,50 +1,166 @@
+#include <any>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <utility>
-#include <vector>
+
+class ObzectItem {
+ public:
+  ObzectItem *pItem;
+  ObzectItem *nItem;
+
+  virtual void setPPtr(ObzectItem *pItem) = 0;
+  virtual void setNPtr(ObzectItem *nItem) = 0;
+  virtual void setPtr(ObzectItem *pItem, ObzectItem *nItem) = 0;
+
+  virtual std::any getKey() = 0;
+  virtual std::any getValue() = 0;
+
+  virtual void displayKey() = 0;
+  virtual void displayValue() = 0;
+};
 
 template <class K, class V>
-class Obzect {
- private:
-  std::vector<std::pair<K, V>> obz;
+class ObzItemExplicit : public ObzectItem {
+ protected:
+  std::pair<K, V> newPair;
+
+  template <typename T>
+  void displayDecorate(T item) {
+    if (typeid(T) == typeid(std::string)) {
+      std::cout << "\"" << item << "\"";
+    } else {
+      std::cout << item;
+    }
+    return;
+  }
 
  public:
-  Obzect() {}
-  Obzect(K &key, V &val) {}
+  ObzItemExplicit(const K &key, const V &val) {
+    this->newPair.first = key;
+    this->newPair.second = val;
 
-  // friend std::ostream &operator<<(std::ostream &, Obzect<K, V> &);
+    this->pItem = nullptr;
+    this->nItem = nullptr;
+  }
 
-  // "id": 9
+  void setPPtr(ObzectItem *pItem) { this->pItem = pItem; }
+  void setNPtr(ObzectItem *nItem) { this->nItem = nItem; }
+  void setPtr(ObzectItem *pItem, ObzectItem *nItem) {
+    this->pItem = pItem;
+    this->nItem = nItem;
+  }
+
+  std::any getKey() { return this->newPair.first; }
+  std::any getValue() { return this->newPair.second; }
+
+  void displayKey() { this->displayDecorate(this->newPair.first); }
+  void displayValue() { this->displayDecorate(this->newPair.second); }
+};
+
+class Obzect {
+ protected:
+  ObzectItem *head;
+  ObzectItem *tail;
+
+  ObzectItem *cursor;
+
+ public:
+  Obzect() : head(nullptr), tail(nullptr) {}
+
+  template <typename K, typename V>
   void push(const K &key, const V &val) {
-    std::pair<K, V> *newPair = new std::pair<K, V>;
-    newPair->first = key;
-    newPair->second = val;
+    ObzItemExplicit<K, V> *newItem = new ObzItemExplicit<K, V>(key, val);
 
-    this->obz.push_back(*newPair);
+    if (this->head == nullptr) {
+      this->head = newItem;
+    } else {
+      this->tail->setNPtr(newItem);
+    }
+
+    newItem->setPtr(this->tail, nullptr);
+    this->tail = newItem;
 
     return;
   }
 
-  // "user": "horrifyinghorse",
-  void push(K &key, V *val) {
-    std::pair<K, V *> newPair = new std::pair<K, V *>;
+  void push(const char *key, const char *val) {
+    this->push(std::string(key), std::string(val));
   }
+
+  template <typename V>
+  void push(const char *key, V val) {
+    this->push(std::string(key), val);
+  }
+
+  template <typename K>
+  void push(K key, const char *val) {
+    this->push(key, std::string(val));
+  }
+
+  template <typename K>
+  bool find(const K &key) {
+    ObzectItem *it = this->head;
+    while (it) {
+      // std::cout << "Ret Type: " << it->getKey().type().name();
+
+      this->cursor = it;
+
+      if (it->getKey().type() == typeid(key)) {
+        K itKey = std::any_cast<K>(it->getKey());
+        if (itKey == key) {
+          return 1;
+        }
+      }
+      it = it->nItem;
+    }
+    return 0;
+  }
+
+  bool find(const char *key) { return this->find(std::string(key)); }
+
+  template <typename V, typename K>
+  V key(const K &key) {
+    if (!this->find(key)) {
+      std::cerr << "Obzect cannot find the key specified: `" << key << "`\n";
+      exit(1);
+    }
+
+    return std::any_cast<V>(this->cursor->getValue());
+  }
+
+  template <typename V, typename K>
+  V key(const char *key) {
+    return this->key<V, std::string>(std::string(key));
+  }
+
+  //  template <typename K>
+  //  std::any key(const K &key) {
+  //    if (!this->find(key)) {
+  //      std::cerr << "\nObzect cannot find the key specified: `" << key <<
+  //      "`\n"; exit(1);
+  //    }
+  //
+  //    return this->cursor->getValue();
+  //  }
 
   void display() {
     int decorateSpc = 2;
-    typename std::vector<std::pair<K, V>>::iterator it;
+    ObzectItem *it = this->head;
 
-    std::cout << "\n";
-    std::cout << "{\n";
-    for (it = obz.begin(); it != obz.end(); ++it) {
+    std::cout << "\n{\n";
+    while (it) {
       int spc = decorateSpc;
       while (spc--) {
         std::cout << " ";
       }
 
-      std::cout << (*it).first << ": \"" << (*it).second << "\"";
+      it->displayKey();
+      std::cout << ": ";
+      it->displayValue();
+      std::cout << ",\n";
 
-      std::cout << "\n";
+      it = it->nItem;
     }
     std::cout << "}\n";
 
@@ -52,30 +168,26 @@ class Obzect {
   }
 };
 
-// template <typename K, typename V>
-// std::ostream &operator<<(std::ostream &os, const Obzect<K, V> &o) {
-//   std::cout << "shit";
-//
-//   return os;
-// }
-
 int main() {
-  Obzect<std::string, std::string> o;
-  Obzect<std::string, int> o2;
+  Obzect o1;
+  o1.push("Name", "Ayaan");
+  o1.push("Username", "Horrifyinghorse");
+  o1.push("ID", 28);
+  o1.push(5, 9);
 
-  o.push("yes", "13");
-  o.push("user", "09");
-  o.push("id", "05");
+  o1.display();
 
-  std::cout << "O1:\n";
-  o.display();
+  if (o1.find("ID")) {
+    std::cout << "Find return YES!";
+  } else {
+    std::cout << "Find return No!";
+  }
 
-  o2.push("yes", 13);
-  o2.push("user", 9);
-  o2.push("id", 5);
+  std::cout << "\nUsing key:";
+  std::cout << "\nUsername = " << o1.key<std::string>("Name");
+  std::cout << "\nID = " << o1.key<int>("ID");
 
-  std::cout << "\nO2:";
-  o2.display();
+  std::cout << "\n5 = " << o1.key<int>(5);
 
   return 0;
 }
