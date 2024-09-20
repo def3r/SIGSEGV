be67c471760960a9943986727a1f3c4b478e8fe5
horrifyingHorse <excuseplanation@gmail.com>
2024-09-20 23:01:24 +0530

    now, to the pointers in kvPair

--- obzect.cpp
+++ obzect.cpp
@@ -1,7 +1,7 @@
 #include <any>
-#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <type_traits>
 #include <utility>
 
 class ObzectItem {
@@ -9,12 +9,19 @@ class ObzectItem {
   ObzectItem *pItem;
   ObzectItem *nItem;
 
+  virtual ~ObzectItem() {}
+
   virtual void setPPtr(ObzectItem *pItem) = 0;
   virtual void setNPtr(ObzectItem *nItem) = 0;
   virtual void setPtr(ObzectItem *pItem, ObzectItem *nItem) = 0;
 
   virtual std::any getKey() = 0;
+  virtual std::any getKeyPtr() = 0;
+  virtual bool setKey(const std::any &) = 0;
+
   virtual std::any getValue() = 0;
+  virtual std::any getValuePtr() = 0;
+  virtual bool setValue(const std::any &) = 0;
 
   virtual void displayKey() = 0;
   virtual void displayValue() = 0;
@@ -23,22 +30,23 @@ class ObzectItem {
 template <class K, class V>
 class ObzItemExplicit : public ObzectItem {
  protected:
-  std::pair<K, V> newPair;
+  std::pair<K, V> kvPair;
 
   template <typename T>
-  void displayDecorate(T item) {
-    if (typeid(T) == typeid(std::string)) {
-      std::cout << "\"" << item << "\"";
-    } else {
-      std::cout << item;
+  T decorate(T item) {
+    if constexpr (std::is_same<T, std::string>::value) {
+      return "\"" + item + "\"";
     }
-    return;
+    return item;
   }
+
+  K decorateKey() { return this->decorate(kvPair.first); }
+  V decorateValue() { return this->decorate(kvPair.second); }
 
  public:
   ObzItemExplicit(const K &key, const V &val) {
-    this->newPair.first = key;
-    this->newPair.second = val;
+    this->kvPair.first = key;
+    this->kvPair.second = val;
 
     this->pItem = nullptr;
     this->nItem = nullptr;
@@ -51,11 +59,42 @@ class ObzItemExplicit : public ObzectItem {
     this->nItem = nItem;
   }
 
-  std::any getKey() { return this->newPair.first; }
-  std::any getValue() { return this->newPair.second; }
+  std::any getKey() { return this->kvPair.first; }
 
-  void displayKey() { this->displayDecorate(this->newPair.first); }
-  void displayValue() { this->displayDecorate(this->newPair.second); }
+  std::any getKeyPtr() { return &this->kvPair.first; }
+
+  bool setKey(const std::any &key) {
+    if (key.type() != typeid(K)) {
+      std::cerr << "\nNew key type(" << key.type().name()
+                << ") differs from init key(" << this->decorateKey()
+                << ") type(" << typeid(this->kvPair.first).name() << ")";
+      return false;
+    }
+
+    this->kvPair.first = std::any_cast<K>(key);
+
+    return true;
+  }
+
+  std::any getValue() { return this->kvPair.second; }
+
+  std::any getValuePtr() { return &this->kvPair.second; }
+
+  bool setValue(const std::any &val) {
+    if (val.type() != typeid(V)) {
+      std::cerr << "\nNew value type(" << val.type().name()
+                << ") differs from init val(" << this->decorateValue()
+                << ") type(" << typeid(this->kvPair.second).name() << ")";
+      return false;
+    }
+
+    this->kvPair.second = std::any_cast<V>(val);
+
+    return true;
+  }
+
+  void displayKey() { std::cout << this->decorateKey(); }
+  void displayValue() { std::cout << this->decorateValue(); }
 };
 
 class Obzect {
@@ -68,8 +107,22 @@ class Obzect {
  public:
   Obzect() : head(nullptr), tail(nullptr) {}
 
+  ~Obzect() {
+    ObzectItem *it = this->head;
+    while (it) {
+      ObzectItem *del = it;
+      delete del;
+      it = it->nItem;
+    }
+  }
+
   template <typename K, typename V>
   void push(const K &key, const V &val) {
+    if (this->find(key)) {
+      cursor->setValue(std::any(val));
+      return;
+    }
+
     ObzItemExplicit<K, V> *newItem = new ObzItemExplicit<K, V>(key, val);
 
     if (this->head == nullptr) {
@@ -78,8 +131,8 @@ class Obzect {
       this->tail->setNPtr(newItem);
     }
 
-    newItem->setPtr(this->tail, nullptr);
     this->tail = newItem;
+    newItem->setPtr(this->tail, nullptr);
 
     return;
   }
@@ -126,12 +179,28 @@ class Obzect {
       exit(1);
     }
 
+    if constexpr (std::is_same<V, std::any>::value) {
+      return this->cursor->getValue();
+    }
+
     return std::any_cast<V>(this->cursor->getValue());
   }
 
   template <typename V, typename K>
   V key(const char *key) {
     return this->key<V, std::string>(std::string(key));
+  }
+
+  void clear() {
+    ObzectItem *it = this->head;
+    while (it) {
+      ObzectItem *del = it;
+      delete del;
+      it = it->nItem;
+    }
+    this->head = nullptr;
+    this->tail = nullptr;
+    this->tail = nullptr;
   }
 
   //  template <typename K>
@@ -166,6 +235,13 @@ class Obzect {
 
     return;
   }
+
+  template <typename K>
+  std::any operator[](const K &key) {
+    return this->key<std::any>(key);
+  }
+
+  std::any operator[](const char *key) { return this->key<std::any>(key); }
 };
 
 int main() {
@@ -174,6 +250,8 @@ int main() {
   o1.push("Username", "Horrifyinghorse");
   o1.push("ID", 28);
   o1.push(5, 9);
+  o1.push(3.14, "Pi");
+  o1.push(3.14, "22/7");
 
   o1.display();
 
@@ -186,8 +264,16 @@ int main() {
   std::cout << "\nUsing key:";
   std::cout << "\nUsername = " << o1.key<std::string>("Name");
   std::cout << "\nID = " << o1.key<int>("ID");
-
   std::cout << "\n5 = " << o1.key<int>(5);
+
+  std::cout << "\nOverriding name";
+  o1.push("Name", "Khan");
+
+  std::cout << "\nName: " << std::any_cast<int>(o1[5]);
+
+  o1.clear();
+  o1.push("Err", 404);
+  o1.display();
 
   return 0;
 }
