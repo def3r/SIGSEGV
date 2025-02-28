74eee7345b09d22639ef2f5edfa70504d0bd9dfe
horrifyingHorse <excuseplanation@gmail.com>
2025-02-28 22:58:05 +0530

    Render with ftxui

--- CppDump/MarkdownParser.cpp
+++ CppDump/MarkdownParser.cpp
@@ -1,13 +1,20 @@
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+
+#include <ftxui/dom/elements.hpp>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
-class MarkdownRenderer {
-public:
-  MarkdownRenderer() {};
+using namespace ftxui;
 
-  void Parse(const std::string &line) {
+class MarkdownRenderer {
+ public:
+  MarkdownRenderer() { MarkdownRenderer::populateMap(); };
+
+  void Parse(const std::string& line) {
     begin = line.begin();
     for (it = line.begin(); it != line.end(); ++it) {
       if (*it == '*' && prev == '*') {
@@ -29,26 +36,39 @@ public:
   void PrintParsed() {
     for (auto it = parsed.begin(); it != parsed.end(); ++it) {
       std::cout << it->first << "\t\t:\t\t";
-      std::vector<std::string> &styles = it->second;
-      for (auto &style : styles) {
+      std::vector<std::string>& styles = it->second;
+      for (auto& style : styles) {
         std::cout << style << "\t";
       }
       std::cout << "\n";
     }
   }
 
-private:
-  void ParseUtil(const int &offset, const std::string &parsedStyle) {
+  Component getComponent() {
+    auto container = Container::Horizontal({});
+    for (auto it = parsed.begin(); it != parsed.end(); ++it) {
+      auto item = text(it->first);
+      std::vector<std::string>& styles = it->second;
+      for (auto& style : styles) {
+        item |= MarkdownRenderer::decorator[style];
+      }
+      container->Add(Renderer([=] { return item; }));
+    }
+    return container;
+  }
+
+ private:
+  void ParseUtil(const int& offset, const std::string& parsedStyle) {
     updateBegin = true;
     PushText(offset);
-    if (PushFirst(parsedStyle)) {
+    if (ToPush(parsedStyle)) {
       style.push_back(parsedStyle);
     } else {
       style.pop_back();
     }
   }
 
-  void PushText(const int &offset) {
+  void PushText(const int& offset) {
     std::string text =
         (begin >= it - offset) ? "" : std::string(begin, it - offset);
     if (!text.empty()) {
@@ -56,7 +76,7 @@ private:
     }
   }
 
-  bool PushFirst(const std::string &s) {
+  bool ToPush(const std::string& s) {
     if (style.empty())
       return true;
     return (style.back() != s);
@@ -68,13 +88,20 @@ private:
 
   std::vector<std::string> style = {};
   std::vector<std::pair<std::string, std::vector<std::string>>> parsed = {};
+
+  inline static std::unordered_map<std::string, Decorator> decorator;
+  inline static void populateMap() {
+    MarkdownRenderer::decorator["bold"] = bold;
+    MarkdownRenderer::decorator["inverted"] = inverted;
+  }
 };
 
 int main() {
+  auto screen = ScreenInteractive::FitComponent();
   std::string line =
-      "**Classic:`The work` is `mysterious` & `important`** Works?";
+      "Clas**sic: `The work`** is `mysterious` & **`important`** Works?";
   std::cout << line << "\n";
   MarkdownRenderer m;
   m.Parse(line);
-  m.PrintParsed();
+  screen.Loop(m.getComponent());
 }
