49272b6254bc7f84d2c30916ff1341c7b4caa88a
horrifyingHorse <excuseplanation@gmail.com>
2025-04-17 12:39:40 +0530

    tty control handling

--- CppDump/bone/bone.cpp
+++ CppDump/bone/bone.cpp
@@ -1,5 +1,7 @@
 #include <signal.h>
+#include <sys/ioctl.h>
 #include <sys/wait.h>
+#include <termios.h>
 #include <unistd.h>
 #include <chrono>
 #include <csignal>
@@ -44,7 +46,7 @@ typedef struct Compile {
   fs::path srcFile = "";
   fs::path execFile = "";
 
-  bool start() {
+  bool start() const {
     std::string cmd = compiler + " " + srcFile.filename().string() + " -o " +
                       execFile.filename().string();
     if (system(cmd.c_str()) != 0) {
@@ -56,11 +58,67 @@ typedef struct Compile {
   }
 } Compile;
 
-void childOnComplete(pid_t pid) {
-  int status;
-  waitpid(pid, &status, 0);
-  Log::msg("Child exited with status " + std::to_string(status));
-}
+typedef struct Child {
+  pid_t pid;
+  std::string execFile = "";
+
+  bool spawn() {
+    pid = fork();
+    if (pid < 0) {
+      return false;
+    }
+    if (pid == 0) {
+      giveTTY(0, getpid());
+      execl(execFile.c_str(), execFile.c_str(), nullptr);
+      exit(1);
+    }
+
+    giveTTY(pid, pid);
+
+    std::thread([this]() {
+      int status;
+      waitpid(pid, &status, 0);
+      getTTY();
+      Log::msg("Child " + std::to_string(pid) + " exited with status " +
+               std::to_string(status));
+    }).detach();
+
+    return true;
+  }
+
+  bool despawn() {
+    kill(pid, SIGTERM);
+    return true;
+  }
+
+  bool respawn(const Compile& compile) {
+    getTTY();
+    Log::msg("srcFile updated");
+    Log::msg("killing proc " + std::to_string(pid));
+    despawn();
+    compile.start();
+    return spawn();
+  }
+  bool respawn() {
+    getTTY();
+    Log::msg("restarting proc " + std::to_string(pid));
+    despawn();
+    return spawn();
+  }
+
+ private:
+  void giveTTY(pid_t grp, pid_t pid) {
+    setpgid(grp, grp);
+    tcsetpgrp(STDIN_FILENO, pid);
+  }
+
+  void getTTY() {
+    signal(SIGTTOU, SIG_IGN);
+    tcsetpgrp(STDIN_FILENO, getpgrp());
+    tcflush(STDIN_FILENO, TCIFLUSH);
+    signal(SIGTTOU, SIG_DFL);
+  }
+} Child;
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args(argv + 1, argv + argc);
@@ -101,62 +159,25 @@ int main(int argc, char* argv[]) {
     compile.execFile = execFile;
   }
   if (execExists && !fs::exists(execFile)) {
-    Log::error("Needs Compiling");
+    Log::error("No executable found");
     compile.start();
   }
-  close(STDIN_FILENO);
 
-  pid_t pid = fork();
-  std::thread([pid]() {
-    int status;
-    waitpid(pid, &status, 0);
-    std::cout << "Child " << pid << " exited with status " << status << "\n";
-  }).detach();
-  if (pid == 0) {
-    execl(execFile.filename().string().c_str(),
-          execFile.filename().string().c_str(), nullptr);
-    exit(1);
-  }
-
+  Child child = {.execFile = execFile.filename().string()};
+  child.spawn();
   while (1) {
     if (execExists && !fs::exists(execFile)) {
-      Log::msg("srcFile updated");
-      Log::msg("killing proc " + std::to_string(pid));
-      kill(pid, SIGTERM);
-      compile.start();
-      pid = fork();
-      if (pid == 0) {
-        execl(execFile.filename().string().c_str(),
-              execFile.filename().string().c_str(), nullptr);
-      }
-      std::thread([pid]() {
-        int status;
-        waitpid(pid, &status, 0);
-        std::cout << "Child " << pid << " exited with status " << status
-                  << "\n";
-      }).detach();
+      child.respawn(compile);
       continue;
     }
+
     // clang-format off
     if (
       fs::last_write_time(srcFile) > fs::last_write_time(execFile)
     ) {  // clang-format on
-      Log::msg("srcFile updated");
-      Log::msg("killing proc " + std::to_string(pid));
-      kill(pid, SIGTERM);
-      compile.start();
-      pid = fork();
-      if (pid == 0) {
-        execl(execFile.filename().string().c_str(),
-              execFile.filename().string().c_str(), nullptr);
-      }
-      std::thread([pid]() {
-        int status;
-        waitpid(pid, &status, 0);
-        std::cout << "Child " << pid << " exited with status " << status
-                  << "\n";
-      }).detach();
+      child.respawn(compile);
     }
+
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
