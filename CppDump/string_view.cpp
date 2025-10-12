887a72d97f3da549022597cc92430664ac3c7b3b
horrifyingHorse <excuseplanation@gmail.com>
2025-10-12 12:21:29 +0530

    More on string_view

--- CppDump/string_view.cpp
+++ CppDump/string_view.cpp
@@ -34,4 +34,53 @@ int main() {
 
   sv = sv.substr(5, 2);
   std::cout << sv << "\n";
+
+  // The string view should not outlive the pointed string!
+  {
+    std::string s = "Another String View\n";
+    sv = s;
+    std::cout << "Inside: \t" << sv;
+    {
+      std::string_view sv1 = s;
+      sv = sv1;
+      std::cout << "Nested Inside:\t" << sv;  // Valid as string is still alive
+    }
+    std::cout << "Inside: \t" << sv;  // Still in the same scope as the string
+  }
+  std::cout << "Out of Scope:\t" << sv;  // Undefined behavior
+
+  struct Foo {
+    // Constructor copy by val
+    void Assign(std::string s) {
+      sv = s;
+      // copy of s will be destoryed as the func returns
+      // sv outlives s => undefined behavior
+    }
+    void AssignRef(std::string& s) {
+      sv = s;
+      // s is a reference to a string
+      // we have no idea abt the life of string
+      // ub if s is reassigned/destroyed
+    }
+    void Print() { std::cout << sv << std::endl; }
+    std::string_view sv;
+  };
+
+  s = "This is a String View in a Struct!";
+  Foo f;
+  f.Assign(s);
+  f.Print();
+  f.AssignRef(s);
+  f.Print();
+  std::cout << "Reassign s\n";
+  s = "Hello :D";
+  f.Print();
+  {
+    std::string s = "This is inside a temp scope F";
+    f.AssignRef(s);
+    std::cout << "Inside scope: ";
+    f.Print();
+  }
+  std::cout << "Outside scope: ";
+  f.Print();  // undefined behavior
 }
